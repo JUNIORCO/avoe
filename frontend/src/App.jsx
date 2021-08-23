71a281233254ca06046a799b49ebb4dd0d919f84
Sami Junior <sami.jr13@hotmail.com>
2021-08-22 20:53:40 -0400

    added preload images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,28 @@ import Header from './components/Header';
 import Recorder from './components/Recorder';
 import RecorderControls from './components/RecorderControls';
 import StyleSelector from './components/StyleSelector';
+import { Characters } from './constants/characters';
 
 const App = (props) => {
   const { highLevelState } = props;
+
+  useEffect(() => {
+    const characterImages = Characters.map((character) => character.image);
+    cacheImages(characterImages);
+  }, []);
+
+  const cacheImages = async (srcArray) => {
+    const promises = await srcArray.map((src) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = resolve();
+        img.onerror = reject();
+      });
+    });
+
+    await Promise.all(promises);
+  };
 
   return (
     <>
