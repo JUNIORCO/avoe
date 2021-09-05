ef14717e3f5bdf57b5c99c72671f399e876159f4
Sami Junior <sami.jr13@hotmail.com>
2021-09-05 00:27:59 -0400

    open apis

--- frontend/src/apiCalls.js
+++ frontend/src/apiCalls.js
@@ -1,59 +1,13 @@
-const { CloudFunctionsServiceClient } = require('@google-cloud/functions');
-// const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const config = require('./config.json');
 
-// const projectId = google.projectId;
-
-// const getauth = async () => {
-//   const auth = new GoogleAuth({
-//     scopes: 'https://www.googleapis.com/auth/cloud-platform',
-//     credentials: {},
-//   });
-//
-//   const client = await auth.getClient();
-//   const projectId = await auth.getProjectId();
-//   const url = `https://www.googleapis.com/dns/v1/projects/${projectId}`;
-//   const res = await client.request({ url });
-//
-//   let access_token = res.config.headers['Authorization'];
-// };
-
-// import config from './config.json';
+const backendUrl = config.google.backend.url;
 
 export const handleStyleSubmit = async (audioBase64, styleSelected) => {
-  const credentials = {
-    client_email: 'functioninvoker@avoe-323702.iam.gserviceaccount.com',
-    private_key: process.env.INVOKE_WORKFLOW_PRIVATE_KEY,
-  };
-
-  console.log(credentials);
-
-  const cloudFunctionsServiceClient = new CloudFunctionsServiceClient({
-    credentials,
-  });
-
-  const res = await cloudFunctionsServiceClient.callFunction({
-    name: 'invokeWorkflow',
-    data: JSON.stringify({
+  return fetch(backendUrl, {
+    method: 'POST',
+    body: JSON.stringify({
       audioBase64,
-      styleSelected,
-    }),
+      styleSelected
+    })
   });
-
-  console.log(res);
-
-  return res;
-
-  // const res = await fetch(config.google.function.url, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     audioBase64,
-  //     styleSelected,
-  //   }),
-  //   mode: 'no-cors',
-  // });
-
-  // console.log(res);
 };
